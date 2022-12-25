ad5697b26960c021669fcdc89f83bdeff67e0794
geroldrivia <plebanczyk.igor@gmail.com>
2022-12-25 17:02:42 +0100

    Added restart button and improved code

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 const start = document.querySelector(".start");
-const start__button = document.querySelector(".start__button");
-
-start__button.addEventListener("click", () => {
-    start.setAttribute("style", "height: 15vh")
-    start__button.remove();
-    game.setAttribute("style", "visibility: visible");
-});
+const startButton = document.querySelector(".button");
 
 const game = document.querySelector(".game");
 const game__turn = document.querySelector(".game__turn");
+const game__board = document.querySelector(".game__board");
 const game__board__element = document.querySelectorAll(".game__board__element");
 
 let counter = 0;
 let board = new Array(9).fill(null);
+
+startButton.addEventListener("click", () => {
+    start.setAttribute("style", "height: 15vh")
+    startButton.remove();
+    game.setAttribute("style", "visibility: visible");
+});
 
 function createDiv(parentElement, classAttribute) {
     const element = document.createElement("div");
@@ -33,8 +34,8 @@ function checkForWin(board) {
         [2, 4, 6]
     ];
 
-    for (i in combinations) {
-        const [a, b, c] = combinations[i];
+    for (i of combinations) {
+        const [a, b, c] = i;
 
         if ((board[a] === board[b] && board[b] === board[c]) && (board[a] === 0 || board[a] === 1)) {
             game__turn.innerHTML = `Player ${board[a] + 1} wins!`;
@@ -67,14 +68,24 @@ game__board__element.forEach(item => {
         }
         
         if (checkForWin(board)) {
-            game__board__element.forEach(item => item.disabled = true)
+            const restartButton = document.createElement("button");
+            const restartButtonText = document.createElement("span");
+            
+            game__board.remove();
             game__turn.innerHTML = `Player ${(counter - 1) % 2 + 1} wins`;
             game__turn.style.color = ((counter - 1) % 2 + 1) === 1 ? "blue" : "red";
+
+            restartButton.setAttribute("class", "button");
+            restartButton.addEventListener("click", () => { location.reload() });
+            restartButtonText.setAttribute("class", "button__text");
+            restartButtonText.innerHTML = "Restart";
+
+            restartButton.append(restartButtonText);
+            game.append(restartButton);
         } else {
             item.disabled = true;
             game__turn.innerHTML = `Player ${counter % 2 + 1}'s turn`;
             game__turn.style.color = (counter % 2 + 1) === 1 ? "blue" : "red";
         }
-       
     }); 
 });
